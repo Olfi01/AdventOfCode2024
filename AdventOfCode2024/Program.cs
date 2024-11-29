43c86e254bf3b73208b1f67d7794d62cd1b0a85e
Florian Meyer <flom@meyer-alpers.de>
2024-11-29 18:40:29 +0100

    Projektdateien hinzufügen.

---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode2024.Services;
+
+namespace AdventOfCode2024
+{
+    internal class Program
+    {
+        private static InputService inputService;
+
+        static void Main(string[] args)
+        {
+            inputService = new InputService(args[0]);
+        }
+    }
+}
---
+++ AdventOfCode2024/Services/InputService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Services
+{
+    internal class InputService
+    {
+        private readonly HttpClient httpClient;
+        public InputService(string authToken)
+        {
+            CookieContainer container = new();
+            container.Add(new Cookie("session", authToken));
+            HttpClientHandler handler = new() { CookieContainer = container };
+            this.httpClient = new HttpClient(handler);
+        }
+
+        public string getInputAsString(int year, int day)
+        {
+            string url = $"https://adventofcode.com/{year}/day/{day}/input";
+            return httpClient.GetAsync(url).Result;
+        }
+    }
+}
