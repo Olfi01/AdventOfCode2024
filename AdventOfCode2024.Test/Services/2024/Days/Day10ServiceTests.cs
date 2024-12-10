b313cf0ee90945f6265523ccd71e2cdd43da9c22
Olfi01 <flom@meyer-alpers.de>
2024-12-10 15:13:28 +0100

    2024 day 10 (I accidentally solved part 2 when I was still working on part 1, whoops)

---
+++ AdventOfCode2024.Test/Services/2024/Days/Day10ServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+using AdventOfCode2024.Services._2024.Days;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2024.Test.Services;
+using Moq;
+
+namespace AdventOfCode2024.Services._2024.Days.Tests
+{
+    public class Day10ServiceTests : DayServiceTestsBase
+    {
+        private readonly Day10Service service;
+
+        public Day10ServiceTests()
+        {
+            service = new Day10Service(inputServiceMock.Object);
+        }
+
+        [Theory]
+        [InlineData("0123\n1234\n8765\n9876", 1)]
+        [InlineData("89010123\n78121874\n87430965\n96549874\n45678903\n32019012\n01329801\n10456732", 36)]
+        public async Task Part1Test(string input, long expectedOut)
+        {
+            inputServiceMock.Setup(s => s.GetInputAsList(2024, 10, '\n')).ReturnsAsync(input.Split('\n'));
+            Assert.Equal(expectedOut, await service.Part1());
+        }
+
+        [Theory]
+        [InlineData("89010123\n78121874\n87430965\n96549874\n45678903\n32019012\n01329801\n10456732", 81)]
+        public async Task Part2Test(string input, long expectedOut)
+        {
+            inputServiceMock.Setup(s => s.GetInputAsList(2024, 10, '\n')).ReturnsAsync(input.Split('\n'));
+            Assert.Equal(expectedOut, await service.Part2());
+        }
+    }
+}
---
+++ AdventOfCode2024/Services/2024/Days/Day10Service.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Services._2024.Days
+{
+    public class Day10Service(IInputService inputService) : SingleDayService(inputService, 2024, 10)
+    {
+        public async Task<long> Part1()
+        {
+            var input = await InputService.GetInputAsList(year, day);
+            short[,] grid = new short[input.First().Length, input.Count()];
+            var enumerator = input.GetEnumerator();
+            List<(int x, int y)> trailheads = [];
+            for (int y = 0; y < grid.GetLength(1); y++)
+            {
+                enumerator.MoveNext();
+                string line = enumerator.Current;
+                for (int x = 0; x < grid.GetLength(0); x++)
+                {
+                    grid[x, y] = short.Parse(line[x] + "");
+                    if (grid[x, y] == 0) trailheads.Add((x, y));
+                }
+            }
+
+            return trailheads.Sum(t => ListReachablePeaks(t, grid, []).Count);
+        }
+
+        private static readonly (int dx, int dy)[] directions = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+        private static HashSet<(int x, int y)> ListReachablePeaks((int x, int y) trailhead, short[,] grid, HashSet<(int x, int y)> reachablePeaks)
+        {
+            foreach (var (dx, dy) in directions)
+            {
+                int newX = trailhead.x + dx;
+                int newY = trailhead.y + dy;
+                if (newX < 0 || newY < 0 || newX >= grid.GetLength(0) || newY >= grid.GetLength(1)
+                    || grid[newX, newY] != grid[trailhead.x, trailhead.y] + 1) continue;
+                if (grid[newX, newY] == 9) reachablePeaks.Add((newX, newY));
+                else ListReachablePeaks((newX, newY), grid, reachablePeaks);
+            }
+            return reachablePeaks;
+        }
+
+        public async Task<long> Part2()
+        {
+            var input = await InputService.GetInputAsList(year, day);
+            short[,] grid = new short[input.First().Length, input.Count()];
+            var enumerator = input.GetEnumerator();
+            List<(int x, int y)> trailheads = [];
+            for (int y = 0; y < grid.GetLength(1); y++)
+            {
+                enumerator.MoveNext();
+                string line = enumerator.Current;
+                for (int x = 0; x < grid.GetLength(0); x++)
+                {
+                    grid[x, y] = short.Parse(line[x] + "");
+                    if (grid[x, y] == 0) trailheads.Add((x, y));
+                }
+            }
+
+            return trailheads.Sum(t => GetTrailheadRating(t, grid));
+        }
+
+        private static int GetTrailheadRating((int x, int y) trailhead, short[,] grid)
+        {
+            int sum = 0;
+            foreach (var (dx, dy) in directions)
+            {
+                int newX = trailhead.x + dx;
+                int newY = trailhead.y + dy;
+                if (newX < 0 || newY < 0 || newX >= grid.GetLength(0) || newY >= grid.GetLength(1)
+                    || grid[newX, newY] != grid[trailhead.x, trailhead.y] + 1) continue;
+                if (grid[newX, newY] == 9) sum += 1;
+                else sum += GetTrailheadRating((newX, newY), grid);
+            }
+            return sum;
+        }
+    }
+}
