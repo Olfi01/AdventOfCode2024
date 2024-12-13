9fb5363fb55cbd6864c6f27804ecf36f54d6602e
Olfi01 <flom@meyer-alpers.de>
2024-12-13 14:30:42 +0100

    2024, Day 13. I can't believe I took so long for this.

---
+++ AdventOfCode2024.Test/Services/2024/Days/Day13ServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using AdventOfCode2024.Services._2024.Days;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2024.Test.Services;
+using Moq;
+
+namespace AdventOfCode2024.Services._2024.Days.Tests
+{
+    public class Day13ServiceTests : DayServiceTestsBase
+    {
+        private readonly Day13Service service;
+
+        public Day13ServiceTests()
+        {
+            service = new Day13Service(inputServiceMock.Object);
+        }
+
+        [Theory]
+        [InlineData("Button A: X+94, Y+34\nButton B: X+22, Y+67\nPrize: X=8400, Y=5400\n\nButton A: X+26, Y+66\nButton B: X+67, Y+21\nPrize: X=12748, Y=12176\n\nButton A: X+17, Y+86\nButton B: X+84, Y+37\nPrize: X=7870, Y=6450\n\nButton A: X+69, Y+23\nButton B: X+27, Y+71\nPrize: X=18641, Y=10279", 480)]
+        public async Task Part1Test(string input, long expectedOut)
+        {
+            inputServiceMock.Setup(s => s.GetInputAsList(2024, 13, "\n\n")).ReturnsAsync(input.Split("\n\n"));
+            Assert.Equal(expectedOut, await service.Part1());
+        }
+
+        [Theory]
+        [InlineData("Button A: X+94, Y+34\nButton B: X+22, Y+67\nPrize: X=8400, Y=5400\n\nButton A: X+26, Y+66\nButton B: X+67, Y+21\nPrize: X=12748, Y=12176\n\nButton A: X+17, Y+86\nButton B: X+84, Y+37\nPrize: X=7870, Y=6450\n\nButton A: X+69, Y+23\nButton B: X+27, Y+71\nPrize: X=18641, Y=10279", 875318608908)]
+        public async Task Part2Test(string input, long expectedOut)
+        {
+            inputServiceMock.Setup(s => s.GetInputAsList(2024, 13, "\n\n")).ReturnsAsync(input.Split("\n\n"));
+            Assert.Equal(expectedOut, await service.Part2());
+        }
+
+        [Fact]
+        public void ShouldGaussianSolve()
+        {
+            long[,] matrix = new long[2, 3]
+            {
+                { 69, 27, 10000000018641 },
+                { 23, 71, 10000000010279 }
+            };
+            Assert.True(Day13Service.GaussianSolve(matrix));
+            Assert.Equal(102851800151, matrix[0, 2]);
+            Assert.Equal(107526881786, matrix[1, 2]);
+        }
+    }
+}
---
+++ AdventOfCode2024/Services/2024/Days/Day13Service.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Services._2024.Days
+{
+    public partial class Day13Service(IInputService inputService) : SingleDayService(inputService, 2024, 13)
+    {
+        private static readonly Regex MachineInput = new(@"Button A: X\+(?<dxA>\d+), Y\+(?<dyA>\d+)\nButton B: X\+(?<dxB>\d+), Y\+(?<dyB>\d+)\nPrize: X=(?<priceX>\d+), Y=(?<priceY>\d+)");
+
+        private record ClawMachine(long DxA, long DyA, long DxB, long DyB, long PriceX, long PriceY) { }
+
+        public async Task<long> Part1()
+        {
+            var machines = (await InputService.GetInputAsList(year, day, "\n\n")).Select(ReadMachineInput);
+            long sum = 0;
+            foreach (var machine in machines)
+            {
+                /*
+                 * This is my old, ugly-ass code that I used for part 1. I am ashamed of it but want to preserve it in the spirit of transparency.
+                long? lowestCost = null;
+                for (long nA = 0; nA <= 100; nA++)
+                {
+                    if (nA * machine.DxA > machine.PriceX || nA * machine.DyA > machine.PriceY) break;
+                    for (long nB = 0; nB <= 100; nB++)
+                    {
+                        long x = nA * machine.DxA + nB * machine.DxB;
+                        long y = nA * machine.DyA + nB * machine.DyB;
+                        if (x > machine.PriceX ||  y > machine.PriceY)
+                        {
+                            break;
+                        }
+                        else if (x == machine.PriceX && y == machine.PriceY)
+                        {
+                            long cost = 3 * nA + nB;
+                            if (!lowestCost.HasValue || lowestCost.Value > cost)
+                            {
+                                lowestCost = cost;
+                            }
+                            break;
+                        }
+                    }
+                }
+                if (lowestCost.HasValue) sum += lowestCost.Value;
+                */
+
+                // This is what I should have done instead.
+                long[,] matrix = new long[2, 3]
+                {
+                    { machine.DxA, machine.DxB, machine.PriceX },
+                    { machine.DyA, machine.DyB, machine.PriceY }
+                };
+                if (!GaussianSolve(matrix)) continue;
+                sum += matrix[0, 2] * 3 + matrix[1, 2];
+            }
+            return sum;
+        }
+
+        private static ClawMachine ReadMachineInput(string input)
+        {
+            var match = MachineInput.Match(input);
+            return new ClawMachine(
+                long.Parse(match.Groups["dxA"].Value), 
+                long.Parse(match.Groups["dyA"].Value), 
+                long.Parse(match.Groups["dxB"].Value), 
+                long.Parse(match.Groups["dyB"].Value), 
+                long.Parse(match.Groups["priceX"].Value), 
+                long.Parse(match.Groups["priceY"].Value)
+            );
+        }
+
+        public async Task<long> Part2()
+        {
+            var machines = (await InputService.GetInputAsList(year, day, "\n\n")).Select(ReadMachineInputPart2);
+            long sum = 0;
+            foreach (var machine in machines)
+            {
+                long[,] matrix = new long[2, 3]
+                {
+                    { machine.DxA, machine.DxB, machine.PriceX },
+                    { machine.DyA, machine.DyB, machine.PriceY }
+                };
+                if (!GaussianSolve(matrix)) continue;
+                sum += matrix[0, 2] * 3 + matrix[1, 2];
+            }
+            return sum;
+        }
+
+        public static bool GaussianSolve(long[,] matrix)
+        {
+            if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 3) throw new Exception();
+            long lcm = Lcm(matrix[0, 0], matrix[1, 0]);
+            long f0 = lcm / matrix[0, 0];
+            long f1 = lcm / matrix[1, 0];
+            for (int i = 0; i < 3; i++)
+            {
+                matrix[0, i] *= f0;
+                matrix[1, i] *= f1;
+                matrix[1, i] -= matrix[0, i];
+            }
+
+            if (matrix[1, 2] % matrix[1, 1] != 0) return false;
+
+            lcm = Lcm(matrix[0, 1], matrix[1, 1]);
+            f0 = lcm / matrix[0, 1];
+            f1 = lcm / matrix[1, 1];
+            for (int i = 0; i < 3; i++)
+            {
+                matrix[0, i] *= f0;
+                matrix[1, i] *= f1;
+                matrix[0, i] -= matrix[1, i];
+            }
+
+            if (matrix[0, 2] % matrix[0, 0] != 0) return false;
+
+            for (int i = 0; i < 2; i++)
+            {
+                matrix[i, 2] /= matrix[i, i];
+                matrix[i, i] = 1;
+            }
+            return true;
+        }
+
+        // stolen from https://stackoverflow.com/questions/13569810/least-common-multiple
+        private static long Gcf(long a, long b)
+        {
+            while (b != 0)
+            {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        private static long Lcm(long a, long b)
+        {
+            return (a / Gcf(a, b)) * b;
+        }
+
+        private static ClawMachine ReadMachineInputPart2(string input)
+        {
+            var match = MachineInput.Match(input);
+            return new ClawMachine(
+                long.Parse(match.Groups["dxA"].Value),
+                long.Parse(match.Groups["dyA"].Value),
+                long.Parse(match.Groups["dxB"].Value),
+                long.Parse(match.Groups["dyB"].Value),
+                long.Parse(match.Groups["priceX"].Value) + 10000000000000,
+                long.Parse(match.Groups["priceY"].Value) + 10000000000000
+            );
+        }
+    }
+}
